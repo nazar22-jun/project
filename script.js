7292dfe5499a805acf150d43f64a0dc59eb08be7
nazar_yuriichuk <nazarurijcuk76@gmail.com>
2020-04-28 15:04:49 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log(4 / 0);
+
+let person = {
+    'name-name': "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(person["name-name"]);
+let arr = ["fhscb", 'sffv', 'rfv'];
+console.log(arr[0]);
+
+let answer = confirm("are you sure?");
+console.log(answer);
+console.log(typeof (answer));
+console.log(typeof (null));
+let incr = 10,
+    decr = 10;
+incr++;
+decr--;
+console.log(incr);
+console.log(decr);
